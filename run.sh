46e6d14b8f1124ec8c5c65a736c6f7b5ac00d447
claytantor <claytantor@gmail.com>
2019-03-08 11:19:57 -0800

    working bootstrap

--- run.sh
+++ run.sh
@@ -5,15 +5,13 @@ set -e
 THING_NAME=$1
 THING_ENDPOINT=$2
 
-python3 -m venv venv --system-site-packages
 
 if [ $(python3 openssl.py) == "true" ]
 then
 
 echo OpenSSL is enabled.
-./venv/bin/pip install -r requirements.txt
 echo "thing name: ${THING_NAME} thing endpoint: ${THING_ENDPOINT}"
-./venv/bin/python bootstrap.py -e ${THING_ENDPOINT} -r ../root-ca-cert.pem -c ../${THING_NAME}-certificate.pem -k ../${THING_NAME}-keypair-private.pem -m subscribe -t bootstrap
+./venv/bin/python bootstrap.py -e ${THING_ENDPOINT} -r ../root-ca-cert.pem -c ../${THING_NAME}-certificate.pem -k ../${THING_NAME}-keypair-private.pem --mode both --topic sdk/test/Python
 
 else
 echo ERROR: OpenSSL is required fro IOT TLS.
