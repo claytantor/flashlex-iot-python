9665c9c898f7720671232bc47f96265753646956
claytantor <claytantor@gmail.com>
2019-06-30 22:49:27 -0700

    changes for iot namespace

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flashlexpi",
-    version="0.9.1",
+    name="flashlexiot",
+    version="0.9.2",
     author="Clay Graham",
     author_email="claytantor@flashlex.com",
-    description="Flashlex pi for python makes it easy to make your Rasberry Pi an IOT device.",
+    description="Flashlex IOT for python makes it easy to make any python computer an IOT device.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/claytantor/flashlex-pi-python",
+    url="https://github.com/claytantor/flashlexiot-python",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
