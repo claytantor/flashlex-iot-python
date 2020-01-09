75f1b750f02277d5623e0c345ed691394179c97a
claytantor <claytantor@gmail.com>
2020-01-08 23:26:57 -0800

    trying to get this to deploy 0.9.9

--- setup.py
+++ setup.py
@@ -17,15 +17,15 @@ class VerifyVersionCommand(install):
     def run(self):
         tag = os.getenv('CIRCLE_TAG')
 
-        if tag != '0.9.8':
+        if tag != '0.9.9':
             info = "Git tag: {0} does not match the version of this app: {1}".format(
-                tag, '0.9.8'
+                tag, '0.9.9'
             )
             sys.exit(info)
 
 setuptools.setup(
     name="flashlexiot",
-    version="0.9.8",
+    version="0.9.9",
     author="Clay Graham",
     author_email="claytantor@flashlex.com",
     description="Flashlex IOT for python makes it easy to make any python computer an IOT device.",
