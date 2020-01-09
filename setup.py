81160419f4c207959c1aa8c4ffd6d66dbf452977
claytantor <claytantor@gmail.com>
2020-01-08 20:37:20 -0800

    adding pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flashlexiot",
-    version="0.9.2",
+    version="0.9.3",
     author="Clay Graham",
     author_email="claytantor@flashlex.com",
     description="Flashlex IOT for python makes it easy to make any python computer an IOT device.",
@@ -14,8 +14,14 @@ setuptools.setup(
     url="https://github.com/claytantor/flashlex-iot-python",
     packages=setuptools.find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-    ],
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 
 )
