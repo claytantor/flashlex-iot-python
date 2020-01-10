b4ddb59d9f7c9645f8df7961c0d54e641f177266
claytantor <claytantor@gmail.com>
2020-01-09 23:49:44 -0800

    tag to 15

--- scripts/copydist.sh
+++ scripts/copydist.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 dt_timestamp=$(date +%s)
-SERVERLESS_STAGE="dev"
+zipball="master"
+DIST_STAGE="dev"
 
-if [[ ${CIRCLE_BRANCH} == "master" ]]; then
-    SERVERLESS_STAGE="prd"
+if [ -n "$CIRCLE_TAG" ]
+then
+    zipball=$CIRCLE_TAG
+elif [ -n "$CIRCLE_BRANCH" ]
+then
+    zipball=$CIRCLE_BRANCH
+    if [[ ${CIRCLE_BRANCH} == "master" ]]; then
+        DIST_STAGE="prd"
+    fi
 fi
 
-bucket_path="s3://github.com.flashlex-iot-python/$SERVERLESS_STAGE/"
+bucket_path="s3://github.com.flashlex-iot-python/$DIST_STAGE/"
 file="${REPO}-${CIRCLE_SHA1}.zip"
 
 aws iam get-user
-wget -O "${REPO}-${CIRCLE_SHA1}.zip" "https://github.com/claytantor/flashlex-iot-python/zipball/${CIRCLE_BRANCH}/"
+wget -O "${REPO}-${CIRCLE_SHA1}.zip" "https://github.com/claytantor/flashlex-iot-python/zipball/${zipball}/"
 aws s3 cp $file $bucket_path 
