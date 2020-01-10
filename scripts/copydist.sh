548f1aea06d85efbb1c86cf3bc90614e956363b7
claytantor <claytantor@gmail.com>
2020-01-09 22:28:35 -0800

    remove the pathing

--- scripts/copydist.sh
+++ scripts/copydist.sh
@@ -4,10 +4,9 @@ dt_timestamp=$(date +%s)
 git_branchname=$(git branch | grep \* | cut -d ' ' -f2)
 git_hash=$(git rev-parse --verify HEAD)
 
-bucket_path="s3://github.com.flashlex-iot-python/$SERVERLESS_STAGE"
-file="build/${REPO}-${TRAVIS_COMMIT}.zip"
+bucket_path="s3://github.com.flashlex-iot-python/$SERVERLESS_STAGE/"
+file="${REPO}-${TRAVIS_COMMIT}.zip"
 
 aws iam get-user
-mkdir build
 wget -O "${REPO}-${TRAVIS_COMMIT}.zip" "https://github.com/claytantor/flashlex-iot-python/zipball/${git_branchname}/"
 aws s3 cp $file $bucket_path 
