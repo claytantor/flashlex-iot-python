b701b5ea2071a8fca979c4053508613b4909b94f
claytantor <claytantor@gmail.com>
2020-01-09 23:24:09 -0800

    tag to 13

--- scripts/copydist.sh
+++ scripts/copydist.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 dt_timestamp=$(date +%s)
-git_branchname=$(git branch | grep \* | cut -d ' ' -f2)
-git_hash=$(git rev-parse --verify HEAD)
 
 bucket_path="s3://github.com.flashlex-iot-python/$SERVERLESS_STAGE/"
-file="${REPO}-${TRAVIS_COMMIT}.zip"
+file="${REPO}-${CIRCLE_SHA1}.zip"
 
 aws iam get-user
-wget -O "${REPO}-${TRAVIS_COMMIT}.zip" "https://github.com/claytantor/flashlex-iot-python/zipball/${git_branchname}/"
+wget -O "${REPO}-${CIRCLE_SHA1}.zip" "https://github.com/claytantor/flashlex-iot-python/zipball/${CIRCLECI_BRANCH}/"
 aws s3 cp $file $bucket_path 
